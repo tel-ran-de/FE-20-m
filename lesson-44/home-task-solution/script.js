04e93bb0574a3ea58c25eed445d6b98fe999e3d0
LeSitar <lenasitars@gmail.com>
2021-08-24 10:24:57 +0200

    lesson-44-update

--- lesson-44/home-task-solution/script.js
+++ lesson-44/home-task-solution/script.js
@@ -1,20 +1,22 @@
 const contacts = [
-    {name:'John Doe',phone:'123456789',email:'john@mail.com',address:'Tel Aviv',desc:'Best friend'},
-    {name:'Jack Sparrow',phone:'0987654321',email:'jack@mail.com',address:'Carribian Sea',desc:'Pirate'},
-    {name:'Tony Stark',phone:'4566778',email:'tony@stark.com',address:'New York',desc:'IronMen'}
+    { name: 'John Doe', phone: '123456789', email: 'john@mail.com', address: 'Tel Aviv', desc: 'Best friend' },
+    { name: 'Jack Sparrow', phone: '0987654321', email: 'jack@mail.com', address: 'Carribian Sea', desc: 'Pirate' },
+    { name: 'Tony Stark', phone: '4566778', email: 'tony@stark.com', address: 'New York', desc: 'IronMen' }
 ];
 
 const root = document.querySelector('#root'),
-      nav = document.querySelector('.nav');
+    nav = document.querySelector('.nav'),
+    searchInput = nav.querySelector('.search input'),
+    searchBtn = nav.querySelector('#search-btn');
 
-let currentPageLink =  document.querySelector('a[href= "contacts"]'),
+let currentPageLink = document.querySelector('a[href= "contacts"]'),
     currentContactRow;
 
-renderContacts();
+renderContacts(contacts);
 
-nav.onclick = function(event){
+nav.onclick = function (event) {
     event.preventDefault();
-    if(event.target.tagName ==='A'){
+    if (event.target.tagName === 'A') {
         currentPageLink.classList.remove('active');
         currentPageLink = event.target;
         currentPageLink.classList.add('active');
@@ -22,32 +24,68 @@ nav.onclick = function(event){
     }
 }
 
-function navigation(path){
-    switch (path){
+searchInput.oninput = function () {
+    const filterContact = contacts.filter((item) =>
+        item.name.toLowerCase().startsWith(searchInput.value.toLowerCase()));
+    renderContacts(filterContact);
+}
+
+/* searchInput.addEventListener('keydown', (e)=>{
+    if(e.key === 'Enter'){
+    const name = searchInput.value.toLowerCase();
+    const filterContact = contacts.filter((item) =>
+    item.name.toLowerCase().startsWith(name));
+    renderContacts(filterContact);
+    }
+}) */
+
+
+searchBtn.onclick = function () {
+    const city = searchInput.value.toLowerCase();
+    const contactsByCity = contacts.filter(item => item.address.toLowerCase() === city);
+    searchInput.value = '';
+    renderContacts(contactsByCity);
+}
+
+document.body.onkeydown = function (e) {
+    if (e.key === 'Enter') {
+        const city = searchInput.value.toLowerCase();
+        const contactsByCity = contacts.filter(item => item.address.toLowerCase() === city);
+        searchInput.value = '';
+        renderContacts(contactsByCity);
+    }
+
+}
+
+function navigation(path) {
+    switch (path) {
         case 'contacts':
-            renderContacts();
+            renderContacts(contacts);
             break;
         case 'addContact':
             renderAddContact();
-            break; 
+            break;
         case 'registration':
             renderRegistartionForm();
-            break;       
+            break;
     }
 }
 
-function renderContacts(){
+function renderContacts(array) {
     root.className = 'contacts';
     root.innerHTML = `
         <ul class = 'list'>
-         ${contacts.map(mapToContactRow).join('')}
+         ${array.map(mapToContactRow).join('')}
          </ul>
          <div class = 'contact-view'>No selected contact</div>
-    `; 
-    root.querySelector('.list').onclick = contactClickHandler;
+    `;
+    root.querySelector('.list').onclick = (event)=>{
+        contactClickHandler(event, array);
+    }
+    
 }
 
-function renderAddContact(){
+function renderAddContact() {
     root.className = 'add-contact';
     root.innerHTML = `
     <form name = "addContactForm" action="#">  
@@ -94,15 +132,15 @@ function renderAddContact(){
     console.log(form)
 
     form.onsubmit = formSubmitHandler;
-    
+
 }
 
-function renderRegistartionForm(){
+function renderRegistartionForm() {
     root.innerHTML = 'registration'
 }
 
-function mapToContactRow(contact, index){
-    return`
+function mapToContactRow(contact, index) {
+    return `
     <li class = 'list-item' data-index = ${index}>
         <h2 class = 'title'>${contact.name}</h2>
         <h3 class = 'sub-title'>${contact.phone}</h3>
@@ -111,29 +149,47 @@ function mapToContactRow(contact, index){
     `
 }
 
-function contactClickHandler(event){
+/* function contactClickHandler(event) {
     let li = event.target;
     console.log(event.target)
-    if(li.classList.contains('delete')){
+    if (li.classList.contains('delete')) {
         contacts.splice(li.parentNode.dataset.index, 1);
-        renderContacts();
-    }else{
-        if(li.tagName !=='LI'){
+        renderContacts(contacts);
+    } else {
+        if (li.tagName !== 'LI') {
             li = event.target.parentNode;
         }
-        if(currentContactRow){
+        if (currentContactRow) {
             currentContactRow.classList.remove('item-active')
         }
         showContact(li.dataset.index);
         currentContactRow = li;
         currentContactRow.classList.add('item-active')
     }
+} */
+
+function contactClickHandler(event, array) {
+    let li = event.target;
+    if (li.classList.contains('delete')) {
+        array.splice(li.parentNode.dataset.index, 1)
+        renderContacts(contacts)
+    } else {
+        if (li.tagName !== 'LI') {
+            li = event.target.parentNode
+        }
+        if (currentContactRow) {
+            currentContactRow.classList.remove('item-active')
+        }
+        showContact(li.dataset.index, array)
+        currentContactRow = li
+        currentContactRow.classList.add('item-active')
+    }
 }
 
-function showContact (index){
-    const contact =  contacts[+index];
+/* function showContact(index) {
+    const contact = contacts[+index];
     const contactView = root.querySelector('.contact-view');
-    contactView.innerHTML =`
+    contactView.innerHTML = `
     <h2>${contact.name}</h2>
     <div class="contact-view-row">
       <img src="./img/technology.png" alt="" />
@@ -149,29 +205,50 @@ function showContact (index){
      </div>
      <p>${contact.desc}</p>
     `;
+} */
+
+function showContact(index, array) {
+    const contact = array[+index];
+    const contactView = root.querySelector('.contact-view')
+    contactView.innerHTML = `
+    <h2>${contact.name}</h2>
+    <div class="contact-view-row">
+    <img src="./img/technology.png" alt="" />
+    <h3>${contact.phone}</h3>
+    </div>
+    <div class="contact-view-row">
+    <img src="./img/multimedia.png" alt="" />
+    <h3>${contact.email}</h3>
+    </div>
+    <div class="contact-view-row">
+    <img src="./img/buildings.png" alt="" />
+    <h3 ">${contact.address}</h3>
+    </div>
+    <p>${contact.desc}</p>
+    `
 }
 
-function formSubmitHandler(event){
+function formSubmitHandler(event) {
     event.preventDefault();
     const form = event.target
     contacts.push({
-       name:form.name.value,
-       phone: form.phone.value,
-       email: form.email.value,
-       address: form.address.value,
-       desc: form.description.value
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        address: form.address.value,
+        desc: form.description.value
     })
     clearForm(form);
     currentPageLink.classList.remove('active')
-    currentPageLink =  document.querySelector('a[href= "contacts"]')
+    currentPageLink = document.querySelector('a[href= "contacts"]')
     currentPageLink.classList.add('active');
     navigation('contacts');
 
 }
 
-function clearForm(form){
-    for(element of form){
-        if(element.localName !== 'button'){
+function clearForm(form) {
+    for (element of form) {
+        if (element.localName !== 'button') {
             element.value = ''
         }
     }
