949b05e5efab00c55f5701d37ed764e4b1a7f141
LeSitar <lenasitars@gmail.com>
2021-10-27 16:52:46 +0200

    update config

--- lesson-77/config/db.config.js
+++ lesson-77/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:"mongodb+srv://admin:Lj2ZXhrydUo5S0om@cluster0.to8oj.mongodb.net/todos?retryWrites=true&w=majority"
+    url:"mongodb+srv://<USER>:<PASSWORD>@cluster0.to8oj.mongodb.net/todos?retryWrites=true&w=majority"
 }
